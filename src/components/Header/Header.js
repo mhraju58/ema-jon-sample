8440242a69eb6250f5abe4b5c469db0a2ccfcce9
Monir <monir33@diit.info>
2022-08-26 13:46:03 +0300

    nav added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,13 @@ const Header = () => {
     return (
         <div className='header'>
             <img src={logo} alt="" />
+            <nav>
+                <a href='/shop'>Shop</a>
+                <a href='/review'>Order Review</a>
+                <a href='/manage'>Inventory</a>
+                <a href='/about'>About</a>
+                <a href='/contact'>Contact</a>
+            </nav>
         </div>
     );
 };
